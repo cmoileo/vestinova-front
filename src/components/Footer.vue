11420e54eb9fca19d54f311777d9ac3f9c515d44
cmoileo <leo.fezard33@gmail.com>
2024-11-22 14:37:35 +0100

    refacto footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="w-fit mx-auto md:px-12 md:py-4 px-8 py-2 rounded-full bg-white z-10">
+    <ul class="flex md:gap-16 gap-8 items-center">
+      <router-link to="/">
+        <li :class="baseIconStyle">
+          <v-icon scale="1.5" name="co-home" />
+        </li>
+      </router-link>
+      <create-item-modal :categories="apiResponseStore.data" />
+      <router-link to="/profile">
+        <li :class="baseIconStyle">
+          <v-icon scale="1.5" name="co-user" />
+        </li>
+      </router-link>
+    </ul>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import CreateItemModal from "@/components/modal/Create-item-modal.vue";
+import {onMounted} from "vue";
+const baseIconStyle = "opacity-50 transition hover:opacity-100 cursor-pointer";
+import { useCategoryStore } from "@/stores/category";
+
+const apiResponseStore = useCategoryStore();
+
+onMounted(() => {
+  apiResponseStore.fetchData();
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,8 @@
 import { onMounted } from "vue";
 import { cookieManager } from "@/service/cookie.service";
 import router from "@/router";
-import CreateItemModal from "@/components/modal/Create-item-modal.vue";
 import { useCategoryStore } from "@/stores/category";
-
-const baseIconStyle = "opacity-50 transition hover:opacity-100 cursor-pointer";
+import Footer from "@/components/Footer.vue";
 
 const apiResponseStore = useCategoryStore();
 
@@ -24,20 +22,6 @@ onMounted(async () => {
     <main class="h-[90%] overflow-y-auto no-scrollbar pb-8 px-4">
       <RouterView />
     </main>
-    <footer class="w-fit mx-auto md:px-12 md:py-4 px-8 py-2 rounded-full bg-white z-10">
-      <ul class="flex md:gap-16 gap-8 items-center">
-        <router-link to="/">
-          <li :class="baseIconStyle">
-            <v-icon scale="1.5" name="co-home" />
-          </li>
-        </router-link>
-        <create-item-modal :categories="apiResponseStore.data" />
-        <router-link to="/profile">
-          <li :class="baseIconStyle">
-            <v-icon scale="1.5" name="co-user" />
-          </li>
-        </router-link>
-      </ul>
-    </footer>
+    <Footer />
   </div>
 </template>
